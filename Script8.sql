3e1e5869fde33c665d22f4dc1c4371612bd0d7ed
pcadic <87199501+pcadic@users.noreply.github.com>
2023-04-11 12:59:40 -0700

    Update Script8.sql
    Fix Indentation

--- Script8.sql
+++ Script8.sql
@@ -3,30 +3,30 @@ GO
 
 /* 1 SAMPLING TESTS */
 
-/* 1.1 Random Sampling from a small table with NEWID()				*/
+/* 1.1 Random Sampling from a small table with NEWID()			*/
 /* Select top 10% of Campaigns rows randomly ordered with NEWID()	*/
 SELECT  TOP 10 PERCENT
-		NEWID() AS [GUID],
-		* 
+        NEWID() AS [GUID], 
+        * 
 FROM [dbo].[Campaigns]
 ORDER BY [GUID];
 GO
 
 
-/* 1.2 Random sampling from a large table with BINARY_CHECKSUM			*/
+/* 1.2 Random sampling from a large table with BINARY_CHECKSUM		*/
 /* Select randomly around 10% of subscribers using BINARY_CHECKSUM()	*/
 SELECT * 
 FROM [dbo].[Subscribers]
 WHERE (ABS(CAST((BINARY_CHECKSUM(*) * RAND()) as int)) % 100) < 10;
 GO
 
-/* 1.3 Repeated Random Sample													*/
+/* 1.3 Repeated Random Sample							*/
 /* Select 10% or products row using a repeated random sample with ROW_NUMBER()	*/
 WITH CTE_RN_Products
 AS
 (
-	SELECT  ROW_NUMBER() OVER (ORDER BY [ProductId]) AS [RowNumber],
-			*
+	SELECT  ROW_NUMBER() OVER (ORDER BY [ProductId]) AS [RowNumber], 
+                *
 	FROM [dbo].[Products]
 )
 SELECT *
@@ -34,15 +34,15 @@ FROM CTE_RN_Products
 WHERE (([RowNumber] * 79 + 19) % 100) < 10;
 GO
 
-/* 1.4 Proportional Stratified Sample						*/
-/* See Excel tab 1.4 Stratified sampling					*/
+/* 1.4 Proportional Stratified Sample				*/
+/* See Excel tab 1.4 Stratified sampling			*/
 /* Pre-analysis : Percentage of Products per GroupName		*/
 SELECT  a.*, b.*, 
-		FORMAT(a.[TotalGroupProducts]*1.0/b.[TotalProducts],'N2') AS [PtGroupName]
+        FORMAT(a.[TotalGroupProducts]*1.0/b.[TotalProducts],'N2') AS [PtGroupName]
 FROM
 (
 	SELECT  [GroupName], 
-			COUNT(*) AS [TotalGroupProducts] 
+                COUNT(*) AS [TotalGroupProducts] 
 	FROM [dbo].[Products]
 	GROUP BY [GroupName]
 ) a
@@ -60,7 +60,7 @@ WITH CTE_OrderedProduct
 AS
 (
 	SELECT  ROW_NUMBER() OVER (ORDER BY [GroupName]) as [RowNumber], 
-			*
+		*
 	FROM [dbo].[Products]
 )
 SELECT * 
@@ -70,15 +70,15 @@ ORDER BY [GroupName];
 GO
 
 
-/* 1.5 Balanced Sample																	*/
-/* See Excel tab 1.5 Balanced sampling Data												*/ 
+/* 1.5 Balanced Sample									*/
+/* See Excel tab 1.5 Balanced sampling Data						*/ 
 /* Pre-analysis : Orders placed in 2015 for more than $1000 during weekdens or weekdays */
 --It seems that the orders with the highest Total Price have been placed during the Weekend
 --Are they just 3 outliers? 
 --Do the highest orders are mainly placed during weekdays ?
-SELECT 	[OrderId], [OrderDate], [TotalPrice],
-		IIF(c.[DOWint] = 1 OR c.[DOWint] = 7,  [TotalPrice], NULL) AS [Weekend],
-		IIF(c.[DOWint] = 1 OR c.[DOWint] = 7,  NULL, [TotalPrice]) AS [NotWeekend]
+SELECT 	[OrderId], [OrderDate], [TotalPrice], 
+        IIF(c.[DOWint] = 1 OR c.[DOWint] = 7,  [TotalPrice], NULL) AS [Weekend], 
+        IIF(c.[DOWint] = 1 OR c.[DOWint] = 7,  NULL, [TotalPrice]) AS [NotWeekend]
 FROM [dbo].[Orders] o
 JOIN [dbo].[Calendar] c ON o.[OrderDate] = c.[Date]
 AND YEAR([OrderDate]) = '2015'
@@ -91,20 +91,20 @@ WITH CTE_IsWeekend
 AS
 (
 	SELECT ROW_NUMBER() OVER (PARTITION BY [IsWeekend] ORDER BY NEWID()) AS [RN_IsWeekend],
-			*
+	       *
 	FROM
 		(
-			SELECT 	[OrderId], [OrderDate], [TotalPrice],
-					IIF(c.[DOWint] = 1 OR c.[DOWint] = 7,  1, 0) AS [IsWeekend]
+			SELECT 	[OrderId], [OrderDate], [TotalPrice], 
+                                IIF(c.[DOWint] = 1 OR c.[DOWint] = 7,  1, 0) AS [IsWeekend]
 			FROM [dbo].[Orders] o
 			JOIN [dbo].[Calendar] c ON o.[OrderDate] = c.[Date]
 			AND YEAR([OrderDate]) = '2015'
 			AND [TotalPrice] > 1000
 		) a
 ) 
-SELECT [OrderId], [OrderDate], [TotalPrice],
-		IIF([IsWeekend] = 1, [TotalPrice], NULL) AS [Weekend],
-		IIF([IsWeekend] = 0, [TotalPrice], NULL) AS [NotWeekend]
+SELECT [OrderId], [OrderDate], [TotalPrice], 
+       IIF([IsWeekend] = 1, [TotalPrice], NULL) AS [Weekend], 
+       IIF([IsWeekend] = 0, [TotalPrice], NULL) AS [NotWeekend]
 FROM CTE_IsWeekend
 WHERE [RN_IsWeekend] <= 50;
 GO
@@ -117,7 +117,7 @@ GO
 /* Model set is year 2014		*/
 /* Score set is year 2015		*/
 SELECT	YEAR(o.[OrderDate]) AS [Year], 
-		AVG(o.[TotalPrice]) AS [Average Total Price F]
+        AVG(o.[TotalPrice]) AS [Average Total Price F]
 FROM [dbo].[Orders] o 
 JOIN [dbo].[Customers] c ON o.[CustomerId] = c.[CustomerId]
 WHERE YEAR(o.[OrderDate]) IN ('2014','2015')
@@ -128,10 +128,10 @@ GO
 /* Dimension : State */
 --State has no incidence on the sales
 DECLARE @FallBack2014Avg FLOAT = (SELECT AVG([TotalPrice]) 
-									FROM [Orders] o
-									JOIN [Customers] c ON o.[CustomerId] = c.[CustomerId] 
-									WHERE YEAR([OrderDate]) = 2014
-										AND c.[Gender] = 'F');
+                                    FROM [Orders] o
+                                    JOIN [Customers] c ON o.[CustomerId] = c.[CustomerId] 
+                                    WHERE YEAR([OrderDate]) = 2014 
+                                    AND c.[Gender] = 'F');
 WITH CTE_Data
 AS
 (
@@ -157,7 +157,7 @@ AS
 	GROUP BY [State]
 )
 SELECT AVG(ctscore.[TotalPrice]) AS [Avg Score TotalPrice], 
-	   AVG(COALESCE(ctmodel.[Average TotalPrice 2014 F], @FallBack2014Avg)) AS [Avg Model TotalPrice]
+       AVG(COALESCE(ctmodel.[Average TotalPrice 2014 F], @FallBack2014Avg)) AS [Avg Model TotalPrice]
 FROM CTE_ScoreSet ctscore
 LEFT JOIN CTE_ModelSet ctmodel ON ctscore.[State] = ctmodel.[State];
 GO
@@ -166,10 +166,10 @@ GO
 /* Dimension : ZipCode */
 --Better but not close
 DECLARE @FallBack2014Avg FLOAT = (SELECT AVG([TotalPrice]) 
-									FROM [Orders] o
-									JOIN [Customers] c ON o.[CustomerId] = c.[CustomerId] 
-									WHERE YEAR([OrderDate]) = 2014
-										AND c.[Gender] = 'F');
+                                    FROM [Orders] o 
+                                    JOIN [Customers] c ON o.[CustomerId] = c.[CustomerId] 
+                                    WHERE YEAR([OrderDate]) = 2014 
+                                    AND c.[Gender] = 'F');
 WITH CTE_Data
 AS
 (
@@ -195,7 +195,7 @@ AS
 	GROUP BY [ZipCode]
 )
 SELECT AVG(ctscore.[TotalPrice]) AS [Avg Score TotalPrice], 
-	   AVG(COALESCE(ctmodel.[Average TotalPrice 2014 F], @FallBack2014Avg)) AS [Avg Model TotalPrice]
+       AVG(COALESCE(ctmodel.[Average TotalPrice 2014 F], @FallBack2014Avg)) AS [Avg Model TotalPrice]
 FROM CTE_ScoreSet ctscore
 LEFT JOIN CTE_ModelSet ctmodel ON ctscore.[ZipCode] = ctmodel.[ZipCode];
 GO
@@ -204,10 +204,10 @@ GO
 /* Dimension : Payment Type */
 --Better but not close
 DECLARE @FallBack2014Avg FLOAT = (SELECT AVG([TotalPrice]) 
-									FROM [Orders] o
-									JOIN [Customers] c ON o.[CustomerId] = c.[CustomerId] 
-									WHERE YEAR([OrderDate]) = 2014
-										AND c.[Gender] = 'F');
+                                    FROM [Orders] o 
+                                    JOIN [Customers] c ON o.[CustomerId] = c.[CustomerId] 
+                                    WHERE YEAR([OrderDate]) = 2014 
+                                    AND c.[Gender] = 'F');
 WITH CTE_Data
 AS
 (
@@ -233,7 +233,7 @@ AS
 	GROUP BY [PaymentType]
 )
 SELECT AVG(ctscore.[TotalPrice]) AS [Avg Score TotalPrice], 
-	   AVG(COALESCE(ctmodel.[Average TotalPrice 2014 F], @FallBack2014Avg)) AS [Avg Model TotalPrice]
+       AVG(COALESCE(ctmodel.[Average TotalPrice 2014 F], @FallBack2014Avg)) AS [Avg Model TotalPrice]
 FROM CTE_ScoreSet ctscore
 LEFT JOIN CTE_ModelSet ctmodel ON ctscore.[PaymentType] = ctmodel.[PaymentType];
 GO
@@ -242,10 +242,10 @@ GO
 /* Dimension : Campaign Channel */
 --Better but not close
 DECLARE @FallBack2014Avg FLOAT = (SELECT AVG([TotalPrice]) 
-									FROM [Orders] o
-									JOIN [Customers] c ON o.[CustomerId] = c.[CustomerId] 
-									WHERE YEAR([OrderDate]) = 2014
-										AND c.[Gender] = 'F');
+                                    FROM [Orders] o 
+                                    JOIN [Customers] c ON o.[CustomerId] = c.[CustomerId] 
+                                    WHERE YEAR([OrderDate]) = 2014 
+                                    AND c.[Gender] = 'F');
 WITH CTE_Data
 AS
 (
@@ -273,7 +273,7 @@ AS
 	GROUP BY c.[Channel]
 )
 SELECT AVG(ctscore.[TotalPrice]) AS [Avg Score TotalPrice], 
-	   AVG(COALESCE(ctmodel.[Average TotalPrice 2014 F], @FallBack2014Avg)) AS [Avg Model TotalPrice]
+       AVG(COALESCE(ctmodel.[Average TotalPrice 2014 F], @FallBack2014Avg)) AS [Avg Model TotalPrice]
 FROM CTE_ScoreSet ctscore
 LEFT JOIN CTE_ModelSet ctmodel ON ctscore.[Channel] = ctmodel.[Channel];
 GO
@@ -283,10 +283,10 @@ GO
 -- Based on the Lesson 
 -- Best Dimensions so far
 DECLARE @FallBack2014Avg FLOAT = (SELECT AVG([TotalPrice]) 
-									FROM [Orders] o
-									JOIN [Customers] c ON o.[CustomerId] = c.[CustomerId] 
-									WHERE YEAR([OrderDate]) = 2014
-										AND c.[Gender] = 'F');
+                                    FROM [Orders] o 
+                                    JOIN [Customers] c ON o.[CustomerId] = c.[CustomerId] 
+                                    WHERE YEAR([OrderDate]) = 2014 
+                                    AND c.[Gender] = 'F');
 WITH CTE_Data
 AS
 (
@@ -314,11 +314,9 @@ AS
 	GROUP BY c.[Channel], d.[PaymentType]
 )
 SELECT AVG(ctscore.[TotalPrice]) AS [Avg Score TotalPrice], 
-	   AVG(COALESCE(ctmodel.[Average TotalPrice 2014 F], @FallBack2014Avg)) AS [Avg Model TotalPrice]
+       AVG(COALESCE(ctmodel.[Average TotalPrice 2014 F], @FallBack2014Avg)) AS [Avg Model TotalPrice]
 FROM CTE_ScoreSet ctscore
-LEFT JOIN CTE_ModelSet ctmodel 
-	ON (ctscore.[Channel] = ctmodel.[Channel] AND
-		ctscore.[PaymentType] = ctmodel.[PaymentType]);
+LEFT JOIN CTE_ModelSet ctmodel ON (ctscore.[Channel] = ctmodel.[Channel] AND ctscore.[PaymentType] = ctmodel.[PaymentType]);
 GO
 
 
@@ -331,9 +329,8 @@ AS
 	JOIN [dbo].[Customers] c ON o.[CustomerId] = c.[CustomerId]
 	WHERE c.[Gender] = 'F'
 )
-SELECT
-   YEAR([OrderDate]) AS [Year], 
-   AVG([TotalPrice]) AS [Average Total Price]
+SELECT YEAR([OrderDate]) AS [Year], 
+       AVG([TotalPrice]) AS [Average Total Price]
 FROM CTE_Data
 GROUP BY YEAR([OrderDate])
 ORDER BY [Year];
@@ -342,10 +339,10 @@ GO
 
 /* Evaluating the Model Using a Value Chart (for Dimensions Campaign Channel and Payment Type) */
 DECLARE @FallBack2014Avg FLOAT = (SELECT AVG([TotalPrice]) 
-									FROM [Orders] o
-									JOIN [Customers] c ON o.[CustomerId] = c.[CustomerId] 
-									WHERE YEAR([OrderDate]) = 2014
-										AND c.[Gender] = 'F');
+                                    FROM [Orders] o 
+                                    JOIN [Customers] c ON o.[CustomerId] = c.[CustomerId] 
+                                    WHERE YEAR([OrderDate]) = 2014 
+                                    AND c.[Gender] = 'F');
 WITH CTE_Data
 AS
 (
@@ -380,11 +377,10 @@ SELECT b.[Decile],
 				SELECT a.*, NTILE(10) OVER (ORDER BY a.[Model TotalPrice] DESC) AS [Decile]
 				FROM (
 						SELECT ctscore.[TotalPrice] AS [Score TotalPrice], 
-							   COALESCE(ctmodel.[Average TotalPrice 2014 F], @FallBack2014Avg) AS [Model TotalPrice]
+						       COALESCE(ctmodel.[Average TotalPrice 2014 F], @FallBack2014Avg) AS [Model TotalPrice]
 						FROM CTE_ScoreSet ctscore
 						LEFT JOIN CTE_ModelSet ctmodel 
-							ON (ctscore.[Channel] = ctmodel.[Channel] AND
-								ctscore.[PaymentType] = ctmodel.[PaymentType])
+							ON (ctscore.[Channel] = ctmodel.[Channel] AND ctscore.[PaymentType] = ctmodel.[PaymentType])
 					) a
 			) b 
 GROUP BY b.[Decile]
